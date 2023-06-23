d2d51e0decb8b308094e868ce7f6d5bf2fc1728b
Pandora <nimfanefor@gmail.com>
2023-06-23 18:51:07 +0600

    изменил модель

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,18 +5,17 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 class Task(models.Model):
     status = models.CharField(max_length=50, null=False, blank=False, verbose_name="Статус", choices=status_choices,
-                              default="new")
+                              default=status_choices[0][0])
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Название")
-    author = models.CharField(max_length=50, null=False, blank=False, verbose_name="Автор", default="Неизвестный")
-    description = models.TextField(max_length=2000, verbose_name="Контент")
-
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    description = models.TextField(max_length=1000, verbose_name="Описание")
+    detailed_description = models.TextField(max_length=2000, verbose_name="Подробное описание", null=True, blank=True,
+                                            default=None)
+    data_field = models.DateField(verbose_name="Дата создания", null=True, blank=True, default=None)
 
     def __str__(self):
-        return f"{self.pk} {self.title}: {self.author}"
+        return f"{self.pk} {self.title}"
 
     class Meta:
         db_table = "tasks"
         verbose_name = "Задача"
-        verbose_name_plural = "Задача"
+        verbose_name_plural = "Задачи"
