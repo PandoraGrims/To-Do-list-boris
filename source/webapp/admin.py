c5491f4a4dd9cb4f260fd21cf2eaa4fae609443f
Pandora <nimfanefor@gmail.com>
2023-06-23 18:50:36 +0600

    обавил поля в админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -4,12 +4,11 @@ from webapp.models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'author', 'created_at']
+    list_display = ['id', 'title', 'data_field', 'status']
     list_display_links = ['id', 'title']
-    list_filter = ['author']
-    search_fields = ['title', 'description']
-    fields = ['title', 'author', 'description', 'created_at', 'updated_at']
-    readonly_fields = ['created_at', 'updated_at']
+    search_fields = ['title', 'description', 'detailed_description']
+    fields = ['title', 'description', 'detailed_description', 'data_field', 'status']
+    readonly_fields = ['data_field']
 
 
 admin.site.register(Task, TaskAdmin)
