9308b46a70cc53718d083fabbb59e723c1667198
Pandora <nimfanefor@gmail.com>
2023-07-07 18:04:47 +0600

    Добавил миграции

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -8,11 +8,10 @@ admin.site.register(Status)
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'data_field', 'status', 'type']
+    list_display = ['id', 'title', 'status']
     list_display_links = ['id', 'title']
     search_fields = ['title', 'detailed_description']
-    fields = ['title', 'detailed_description', 'data_field', 'status']
-    readonly_fields = ['data_field']
+    fields = ['title', 'detailed_description', 'status', 'type']
 
 
 admin.site.register(Task, TaskAdmin)
