b5cfffa2c29e431c65ed8298b211773063920210
Pandora <nimfanefor@gmail.com>
2023-07-07 18:05:08 +0600

    Исправил формы

--- source/webapp/form.py
+++ source/webapp/form.py
@@ -1,11 +1,10 @@
 from django import forms
 
-status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+from webapp.models import Type, Status
 
 
 class TaskForm(forms.Form):
-    status = forms.ChoiceField(required=True, label="Статус", choices=status_choices)
+    status = forms.ModelMultipleChoiceField(queryset=Status.objects.all(), label="Статус")
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Тип")
     title = forms.CharField(max_length=50, required=True, label="Название")
-    description = forms.CharField(max_length=50, required=True, label="Описание")
     detailed_description = forms.CharField(max_length=50, required=True, label="Подробное описание")
-    data_field = forms.DateField(label="Дата создания", required=True)
