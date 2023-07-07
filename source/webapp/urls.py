68ca534307159501fa9e110419a62c5c5b8796da
Pandora <nimfanefor@gmail.com>
2023-07-07 18:06:21 +0600

    Исправил баги в функциях

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from webapp.views import task_list_view, task_update_view, task_create_view, task_view, task_delete_view
+from webapp.views import TaskListView, task_update_view, TaskCreateView, TaskDetailView, task_delete_view
 
 urlpatterns = [
-    path('', task_list_view, name="index"),
-    path('task/add/', task_create_view, name="task_add"),
-    path('task/<int:pk>', task_view, name="task_view"),
+    path('', TaskListView.as_view(), name="index"),
+    path('task/add/', TaskCreateView.as_view(), name="task_add"),
+    path('task/<int:pk>', TaskDetailView.as_view(), name="task_view"),
     path('task/<int:pk>/update', task_update_view, name="task_update_view"),
     path('task/<int:pk>/delete', task_delete_view, name="task_delete_view"),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.form import TaskForm
-from webapp.models import Task, status_choices
+from webapp.models import Task
 
 from django.views import View
 from django.views.generic import TemplateView
@@ -9,55 +9,30 @@ from django.views.generic import TemplateView
 
 class TaskListView(View):
     def get(self, request, *args, **kwargs):
-        tasks = Task.objects.order_by("data_field")
+        tasks = Task.objects.order_by("-status")
         context = {"tasks": tasks}
         return render(request, "index.html", context)
 
 
-def task_list_view(request):
-    tasks = Task.objects.order_by("-data_field")
-    context = {"tasks": tasks}
-    return render(request, "index.html", context)
-
-
-class ArticleCreateView(View):
+class TaskCreateView(View):
     def get(self, request, *args, **kwargs):
-        form = TaskForm()
+        form = TaskForm(data=request.POST)
         print(form)
-        return render(request, "create_task.html", {"status_choices": status_choices, "form": form})
+        return render(request, "create_task.html", {"form": form})
 
     def post(self, request, *args, **kwargs):
         form = TaskForm(data=request.POST)
         if form.is_valid():
-            tags = form.cleaned_data.pop("tags")
             task = Task.objects.create(title=form.cleaned_data.get("title"),
-                                          description=form.cleaned_data.get("description"),
-                                          detailed_description=form.cleaned_data.get("detailed_description"),
-                                          status=form.cleaned_data.get("status"),
-                                          data_field=form.cleaned_data.get("data_field")
-                                          )
-            task.tags.set(tags)
+                                       detailed_description=form.cleaned_data.get("detailed_description"),
+                                       status=form.cleaned_data.get("status"),
+                                       type=form.cleaned_data.get("type"),
+                                       )
+
             return redirect("task_view", pk=task.pk)
         else:
             print(form.errors)
-            return render(request, "create_article.html", {"form": form})
-
-
-def task_create_view(request):
-    if request.method == "GET":
-        form = TaskForm()
-        return render(request, "create_task.html", {"status_choices": status_choices, "form": form})
-    else:
-        form = TaskForm(data=request.POST)
-        if form.is_valid():
-            task = Task.objects.create(title=form.cleaned_data.get("title"),
-                                       description=form.cleaned_data.get("description"),
-                                       detailed_description=form.cleaned_data.get("detailed_description"),
-                                       status=form.cleaned_data.get("status"),
-                                       data_field=form.cleaned_data.get("data_field"))
-            return redirect("task_view", pk=task.pk)
-        else:
-            return render(request, "create_task.html", {"status_choices": status_choices, "form": form})
+            return render(request, "create_task.html", {"form": form})
 
 
 def task_update_view(request, pk):
@@ -67,9 +42,8 @@ def task_update_view(request, pk):
                                  "status": task.status,
                                  "description": task.description,
                                  "detailed_description": task.detailed_description,
-                                 "data_field": task.data_field
                                  })
-        return render(request, "update_task.html", {"status_choices": status_choices, "form": form})
+        return render(request, "update_task.html", {"form": form})
     else:
         form = TaskForm(data=request.POST)
         if form.is_valid():
@@ -77,16 +51,20 @@ def task_update_view(request, pk):
             task.title = request.POST.get("title")
             task.description = request.POST.get("description")
             task.detailed_description = request.POST.get("detailed_description")
-            task.data_field = request.POST.get("data_field")
             task.save()
             return redirect("task_view", pk=task.pk)
         else:
             return render(request, "update_task.html", {"form": form})
 
 
-def task_view(request, *args, pk, **kwargs):
-    task = Task.objects.get(id=pk)
-    return render(request, "task.html", {"task": task})
+class TaskDetailView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["task"] = get_object_or_404(Task, id=kwargs['pk'])
+        return context
+
+    def get_template_names(self):
+        return "task.html"
 
 
 def task_delete_view(request, pk):
