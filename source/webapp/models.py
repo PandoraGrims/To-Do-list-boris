759458dc081602fc3dbcc678c5ec46d545bf0edc
Pandora <nimfanefor@gmail.com>
2023-07-18 02:58:57 +0600

    Добавил новую модель Проект class Project

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    start_date = models.DateField(null=False, blank=False, verbose_name='Дата начала')
+    end_date = models.DateField(null=True, blank=True, verbose_name='Дата окончания')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'projects'
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+
 class Type(models.Model):
     type_name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Тип')
 
